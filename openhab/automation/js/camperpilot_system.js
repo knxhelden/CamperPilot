af6607d758ebcaab557d5cfb34d102e5f3ee8346
Timm Bremus <info@knx-helden.de>
2026-07-17 09:56:38 +0200

    Restructure

--- openhab/automation/js/camperpilot_system.js
+++ openhab/automation/js/camperpilot_system.js
@@ -2,12 +2,12 @@ rules.JSRule({
   name: 'CamperPilot - Shutdown',
   description: 'Fährt openHABian sauber herunter, wenn das Shutdown-Item geschaltet wird.',
   triggers: [
-    triggers.ItemCommandTrigger('System_Shutdown', 'ON')
+    triggers.ItemCommandTrigger('System_CamperPilot_Shutdown', 'ON')
   ],
   execute: (event) => {
     console.warn('Shutdown wurde über openHAB angefordert');
 
-    items.getItem('System_Shutdown').postUpdate('OFF');
+    items.getItem('System_CamperPilot_Shutdown').postUpdate('OFF');
 
     actions.Exec.executeCommandLine(
       '/usr/bin/sudo',
@@ -21,12 +21,12 @@ rules.JSRule({
   name: 'CamperPilot - Reboot',
   description: 'Startet openhabian neu, wenn das Reboot-Item geschaltet wird.',
   triggers: [
-    triggers.ItemCommandTrigger('System_Reboot', 'ON')
+    triggers.ItemCommandTrigger('System_CamperPilot_Reboot', 'ON')
   ],
   execute: (event) => {
     console.warn('Reboot wurde über openHAB angefordert');
 
-    items.getItem('System_Reboot').postUpdate('OFF');
+    items.getItem('System_CamperPilot_Reboot').postUpdate('OFF');
 
     actions.Exec.executeCommandLine(
       '/usr/bin/sudo',
